fe05521838d44fe62add7481e74354a2d2abeb44
sheapa <55968932+sheapa@users.noreply.github.com>
2020-03-28 22:19:18 -0700

    Fixed async logic loop. Added new async handeler funciton for add employee action. Fixed double inquier prompt issue.

--- server.js
+++ server.js
@@ -19,21 +19,23 @@ connection.connect(function(err) {
 if (err) throw err;
 });
 
+// Main menu of user options. Inquierer choice. Select 1 option only.
 function promptUser() {
     return inquirer.prompt([
         {
             type: 'list',
             name: 'choice',
             message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add Employee', 'Add Department', 'Add Role', 'Update Employee']
-        }
+            choices: ['View All Employees', 'Add Employee', 'Add Department', 'Add Role', 'Update Employee', 'EXIT']
+         } 
     ])
 };
 
+// Decision tree following user selection from main menu.
 userChoice = async (answers) => { 
     switch (answers.choice) {
         case 'Add Employee':
-            addEmployee();
+            createEmployee();
             return;
         case 'Add Department':
             addDepartment();
@@ -47,31 +49,56 @@ userChoice = async (answers) => {
         case 'Update Employee':
             employeeUpdate();
             return;
+        case 'EXIT':
+            exitProgram();
+            return;
         default:
             return;
     }
 
 };
 
-addEmployee = () => {
-    function promptUser() {
-        return inquirer.prompt([
-            {
-              type: "input",
-              name: "firstName",
-              message: "New employee's first name?"
+// Employee detail questions fired by "addEmployee()" & "updateEmployee."
+function employeeQuest() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "New employee first name... "
+            },
+        {
+            type: "input",
+            name: "lastName",
+            message: "New employee last name... "
+            },
+        {
+            type: "input",
+            name: "role",
+            message: "New employee role... "
+            },
+        {
+            type: "input",
+            name: "managerId",
+            message: "New employee manager ID#... "
             }
-        ])
+    ])
+};
+
+// Send new employee data to mySQL db.
+insertEmployee = async (employeeData) => {
+    connection.query
+        ('INSERT INTO employee_data (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', 
+            [   
+                employeeData.firstName, 
+                employeeData.lastName,
+                employeeData.role, 
+                employeeData.managerId
+            ], 
+        (err, results) => {
+                if (err) throw err;
+        });
     };
 
-    let answers = await promptUser();
-    // Send user data input to employee_db.mysql.
-    connection.query('INSERT INTO employee_data (first_name) VALUES (?)', [answers.firstName], (err, results) => {
-        if (err)
-            throw err;
-        console.log(results);
-});        
-};
 
 addDepartment = () => {
     console.log('New Department Success...')
@@ -87,18 +114,33 @@ viewAll = () => {
 updateEmployee = () => {
     console.log('Employee MOD Success...')
 }
+exitProgram = () => {
+    console.log("Thank you for choosing SHEAPA employee tracker system! Have a great day!")
+    connection.end();
+}
 
+// Logic loop for managing add employee function and employee data insert.
+async function createEmployee() {
+    try {
+        let employeeData = await employeeQuest();
+        await insertEmployee(employeeData);
+        // Returns user to main menu to select next action.
+        await init();
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+// Async logic loop for entire application.
 async function init() {
     try {
         let answers = await promptUser();
-        console.log(answers.choice);
         await userChoice(answers);
     } catch(err) {
         console.log(err);
     }
 }
 
+// Initiation fucntion. Makes it go.
 init();
-
-// Terminate connection to employee_db.mysql.
-connection.end();
